f178d07d578ef4eeed70f66265af69e410c15a0b
Pasha <nikolenko.pavel@gmail.com>
2022-12-12 21:24:23 +0300

    Начальный вариант.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author authorChekhov = new Author("Антон", "Чехов");
+        Author authorLondon = new Author("Джек", "Лондон");
+
+        Book bookCherryOrchid = new Book("Вишневый сад", authorChekhov, 1985);
+        Book bookSnark = new Book("Путешествие на Снарке", authorLondon, 1911);
+
+        bookCherryOrchid.print();
+        bookSnark.print();
+
+        System.out.println();
+
+        bookSnark.setYear(1980);
+        bookSnark.print();
     }
 }
